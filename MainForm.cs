00a944cbbf27b004d15cec684fe95a5eee55b388
Michael de Radobýl <mj-programming@gmx.de>
2026-07-09 11:30:20 +0200

    Refactor `MainForm.cs` for improved readability

--- MainForm.cs
+++ MainForm.cs
@@ -111,10 +111,10 @@ public partial class MainForm : Form
 
 	/// <summary>Gets the debugger display</summary>
 	/// <returns>The debugger display.</returns>
-
 	private string GetDebuggerDisplay() => ToString();
 
-	/// <param name="text">The text to display in the status bar.</param>
+	/// <summary>Shows the status bar</summary>
+	/// <param name="text">The text to display in the status bar.</param>
 	/// <param name="text">text with some information</param>
 	private void SetStatusBarText(string text)
 	{
